b7f5d36c2b9739699fe1fc0a7599d8fdc47d417f
Bwaweru12345 <bryanwaweru1076@example.com>
2022-12-07 16:01:48 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+28 lines (18 sloc)  544 Bytes
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+function fetchBooks() {
+  // To pass the tests, don't forget to return your fetch!
+  let returnFetch = fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => renderBooks(data))
+  return returnFetch;
+ 
+
+
+}
+
+
+function renderBooks(books) {
+  const main = document.querySelector('main');
+  books.forEach(books => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = books.name;
+    main.appendChild(h2);
+  });
+  
+}
